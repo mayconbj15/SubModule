3c22bde193bc41662a99dfa79582235cd5da189f
mayconbj15 <mayconbj15@gmail.com>
2021-10-20 07:25:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace SubModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./Text.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
